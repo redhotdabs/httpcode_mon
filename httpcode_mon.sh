61ed987a102dd9ac0b687d131a4ac1b6b6fa6ba6
redhotdabs <redhot.xunil@gmail.com>
2017-03-09 23:30:01 +0800

    Update and rename httpcodemon.sh to httpcode_mon.sh

--- httpcode_mon.sh
+++ httpcode_mon.sh
@@ -1,9 +1,7 @@
-httcodemon.sh
+httcode_mon.sh
 
-```
 #!/bin/bash
 # checks every 60 seconds.
 # output format http code then count - http code
 
 watch -n 60 "awk '{ print \$9 }' "$1" | sort -n | uniq -c | awk -v char=\"http code \" '{ print char \$2\": \" \$1 }'"
-```
